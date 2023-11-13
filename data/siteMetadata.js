af3369da084c5db64006dfdb58310b0f1f7cd999
zmy <maeyonzzzz@gmail.com>
2023-11-13 21:25:30 +0800

    modify url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A website used to record my study and work.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.mengyuanzhang.com/',
+  siteRepo: 'https://github.com/Maeyon-Z/my-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
